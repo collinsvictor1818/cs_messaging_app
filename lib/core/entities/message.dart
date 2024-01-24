96242b3cf5cd665cac9873faad05ee1152ab8e50
collinsvictor1818 <collinsvictor1010@gmail.com>
2024-01-24 18:49:57 +0300

    Dependancy Setup: Added all necessary dependancies

---
+++ lib/core/entities/message.dart
@@ -0,0 +1,8 @@
+class Message {
+  final String id;
+  final String content;
+  final String senderId;
+  final DateTime timestamp;
+
+  Message({required this.id, required this.content, required this.senderId, required this.timestamp});
+}
